8f876ac048041e81ba8976a60d4b53da8c55a713
Lai <amelai@au-l2h97dd2kf.home>
2026-09-17 23:07:12 +1000

    Remove page transitions and loading animations

--- app/template.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-import PageMotion from "@/components/page-motion";
-
-export default function Template({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  return <PageMotion key={pathname}>{children}</PageMotion>;
-}
